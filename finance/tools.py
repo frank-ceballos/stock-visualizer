e446e3b16eabff24a94e45c93f871c3dad506742
Frank Ceballos <frank.ceballos123@gmail.com>
2021-02-13 13:40:20 -0600

    a bunch of crap that happened

--- finance/tools.py
+++ finance/tools.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import requests
 
 # To grab stock data
 import yfinance as fyf
@@ -38,3 +39,63 @@ def get_ticker_data(ticker):
     
     # Return
     return df
+
+def get_nasdaq_data():
+    """
+    Grabs NASDAQ data and dumps into a csv. 
+    
+    This was taken from a comment in GitHub and written by Possum. All the
+    credit goes to you bud.
+                
+    Parameters
+    ----------
+    
+    None
+        
+    Example
+    -------
+    
+    
+    Attributes
+    ----------
+    
+    None
+    
+    Author Information
+    ------------------
+    
+    Frank Ceballos:
+    LinkedIn: <https://www.linkedin.com/in/frank-ceballos/>
+
+    Emil Tu
+    GitHub: <https://github.com/Possums>
+    """
+
+
+
+    headers = {
+        'authority': 'api.nasdaq.com',
+        'accept': 'application/json, text/plain, */*',
+        'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36',
+        'origin': 'https://www.nasdaq.com',
+        'sec-fetch-site': 'same-site',
+        'sec-fetch-mode': 'cors',
+        'sec-fetch-dest': 'empty',
+        'referer': 'https://www.nasdaq.com/',
+        'accept-language': 'en-US,en;q=0.9',
+    }
+
+    params = (
+        ('tableonly', 'true'),
+        ('limit', '25'),
+        ('offset', '0'),
+        ('download', 'true'),
+    )
+
+    r = requests.get('https://api.nasdaq.com/api/screener/stocks', 
+    headers=headers, params=params)
+    data = r.json()['data']
+    df = pd.DataFrame(data['rows'], columns=data['headers'])
+    df.to_csv('data/nasdaq_info.csv')
+
+    print('NASDAQ data retrieved.')
